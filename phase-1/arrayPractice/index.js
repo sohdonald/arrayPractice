3093cf6cdc5a33b36e717086b2e330d102f98c2a
Donald Soh <sohdonald@gmail.com>
2023-04-12 17:11:54 -0400

    adding notes

--- phase-1/arrayPractice/index.js
+++ phase-1/arrayPractice/index.js
@@ -4,6 +4,9 @@ const cats = ['Bob','Tanner','Sally'];
 
 function destructivelyAppendCat() {
     cats.push('Gus');
+
+// How is the array cats changed?
+
 }
 
 function destructivelyPrependCat() {
@@ -14,3 +17,29 @@ function destructivelyRemoveLastCat() {
     cats.pop();
 }
 
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    return [...cats, name];
+}
+
+function prependCat(name) {
+    return [name, ...cats]
+
+// what does ... do?
+
+}
+
+function removeLastCat() {
+    return cats.slice(0, cats.length-1);
+}
+
+function removeFirstCat() {
+    return cats.slice(1);
+
+// what is the difference betweedn destructive and non-destructive?
+
+
+}
