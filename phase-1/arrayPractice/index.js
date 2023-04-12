bb30b3fb1f153dbae5e455357b02607c5f4ef81d
Donald Soh <sohdonald@gmail.com>
2023-04-12 16:48:03 -0400

    practicing git

---
+++ phase-1/arrayPractice/index.js
@@ -0,0 +1,16 @@
+// practicing arrays
+
+const cats = ['Bob','Tanner','Sally'];
+
+function destructivelyAppendCat() {
+    cats.push('Gus');
+}
+
+function destructivelyPrependCat() {
+    cats.unshift('Paul');
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
